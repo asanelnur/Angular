43334786547cef876bf0dafa8d971a5020128cb5
Asan Elnur <e_asan@kbtu.kz>
2022-10-21 14:56:35 +0600

    d

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,11 +12,18 @@ import { AppComponent } from './app.component';
 
 import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
+import { DishdetailComponent } from './dishdetail/dishdetail.component';
+import { DishService } from './services/dish.service';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    DishdetailComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
@@ -28,7 +35,7 @@ import { MenuComponent } from './menu/menu.component';
     MatCardModule,
     MatButtonModule
   ],
-  providers: [],
+  providers: [DishService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/dishdetail/dishdetail.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Dish } from '../shared/dish';
+
+
+@Component({
+  selector: 'app-dishdetail',
+  templateUrl: './dishdetail.component.html',
+  styleUrls: ['./dishdetail.component.css']
+})
+export class DishdetailComponent implements OnInit {
+  @Input() dish: Dish = new Dish();
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+}
